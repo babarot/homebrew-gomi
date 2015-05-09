00ad7349cbdf6da25453973a7714ecf60a514ba3
b4b4r07 <b4b4r07@gmail.com>
2015-05-09 14:53:09 +0900

    Add rename action

--- gomi.rb
+++ gomi.rb
@@ -1,28 +1,28 @@
 require "formula"
 
 class Gomi < Formula
-  homepage "https://github.com/b4b4r07/gomi"
-  version '0.1.4'
+    homepage "https://github.com/b4b4r07/gomi"
+    version '0.1.4'
 
-  if Hardware.is_64_bit?
-    url "https://github.com/b4b4r07/gomi/releases/download/v0.1.4/gomi_darwin_amd64"
-    sha1 "5cbbc80f7f97557f2004ce475546f35c78bb79d1"
-  else
-    url "https://github.com/b4b4r07/gomi/releases/download/v0.1.4/gomi_darwin_386"
-    sha1 "53450d3023a9d136d9748bfcdc881d461b862748"
-  end
+    if Hardware.is_64_bit?
+        url "https://github.com/b4b4r07/gomi/releases/download/v0.1.4/gomi_darwin_amd64"
+        sha1 "5cbbc80f7f97557f2004ce475546f35c78bb79d1"
+        system 'mv', 'gomi_darwin_amd64', 'gomi'
+    else
+        url "https://github.com/b4b4r07/gomi/releases/download/v0.1.4/gomi_darwin_386"
+        sha1 "53450d3023a9d136d9748bfcdc881d461b862748"
+        system 'mv', 'gomi_darwin_386', 'gomi'
+    end
 
-  depends_on :arch => :intel
+    depends_on :arch => :intel
 
-  def install
-    bin.install 'gomi'
-  end
+    def install
+        bin.install 'gomi'
+    end
 
-  def caveats
-    msg = <<-'EOF'
-
-    Welcome to gomi
-
-EOF
-  end
+    def caveats
+        msg = <<-'EOF'
+        Welcome to gomi
+        EOF
+    end
 end
