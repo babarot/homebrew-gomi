9a5ff6ffd0b1817f5cb01579e16bd17bcd84399c
b4b4r07 <b4b4r07@gmail.com>
2015-05-09 14:41:08 +0900

    Fix 404

--- gomi.rb
+++ gomi.rb
@@ -5,10 +5,10 @@ class Gomi < Formula
   version '0.1.4'
 
   if Hardware.is_64_bit?
-    url "https://github.com/b4b4r07/gomi/releases/download/v0.1.4/gomi_darwin_amd64.zip"
+    url "https://github.com/b4b4r07/gomi/releases/download/v0.1.4/gomi_darwin_amd64"
     sha1 "3543835615a15d55f4b2899a6f0889ce94d029baebae9ab818d70166e1110fea"
   else
-    url "https://github.com/b4b4r07/gomi/releases/download/v0.1.4/gomi_darwin_386.zip"
+    url "https://github.com/b4b4r07/gomi/releases/download/v0.1.4/gomi_darwin_386"
     sha1 "5f75c79221e73d5aee7c18a67d51dd39384f378fdf7b3a13445578f9ae16f0d4"
   end
 
