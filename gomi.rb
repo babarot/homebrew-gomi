c68b6a103796de7980d560612ac58f0542bf0c7d
b4b4r07 <b4b4r07@gmail.com>
2015-05-09 14:56:08 +0900

    Add rename action

--- gomi.rb
+++ gomi.rb
@@ -7,16 +7,15 @@ class Gomi < Formula
     if Hardware.is_64_bit?
         url "https://github.com/b4b4r07/gomi/releases/download/v0.1.4/gomi_darwin_amd64"
         sha1 "5cbbc80f7f97557f2004ce475546f35c78bb79d1"
-        system 'mv', 'gomi_darwin_amd64', 'gomi'
     else
         url "https://github.com/b4b4r07/gomi/releases/download/v0.1.4/gomi_darwin_386"
         sha1 "53450d3023a9d136d9748bfcdc881d461b862748"
-        system 'mv', 'gomi_darwin_386', 'gomi'
     end
 
     depends_on :arch => :intel
 
     def install
+        system 'mv', "gomi_darwin_*", 'gomi'
         bin.install 'gomi'
     end
 
