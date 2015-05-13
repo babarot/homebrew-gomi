881f315127e1611407f50d388edfff8415ca9405
b4b4r07 <b4b4r07@gmail.com>
2015-05-14 00:23:41 +0900

    v0.1.5

--- gomi.rb
+++ gomi.rb
@@ -2,30 +2,32 @@ require "formula"
 
 class Gomi < Formula
     homepage "https://github.com/b4b4r07/gomi"
-    version '0.1.4'
-
-    if Hardware.is_64_bit?
-        url "https://github.com/b4b4r07/gomi/releases/download/v0.1.4/gomi_darwin_amd64"
-        sha1 "5cbbc80f7f97557f2004ce475546f35c78bb79d1"
-        KIND="amd64"
-    else
-        url "https://github.com/b4b4r07/gomi/releases/download/v0.1.4/gomi_darwin_386"
-        sha1 "53450d3023a9d136d9748bfcdc881d461b862748"
-        KIND="386"
-    end
+    version "0.1.5"
+    url "https://github.com/b4b4r07/gomi/releases/download/v#{version}/gomi"
+    sha1 "2b3b5db55a6f6789fe8a649cc56f89f0fd487640"
 
     def install
-        system 'mv', "gomi_darwin_#{KIND}", 'gomi'
         bin.install 'gomi'
         system "curl -L https://raw.githubusercontent.com/b4b4r07/gomi/master/completions/zsh/_gomi >_gomi"
         zsh_completion.install "_gomi"
-        system "curl -L https://github.com/sindresorhus/osx-trash/releases/download/1.0.0/trash.zip -o trash.zip"
-        system "unzip trash.zip"
-        bin.install 'trash' => 'osx-trash'
+    end
+
+    test do
+        system "#{bin}/gomi", "--help"
     end
 
     def caveats; <<-EOS.undent
-        Welcome to gomi
+                       _ 
+                      (_)
+  __ _  ___  _ __ ___  _ 
+ / _` |/ _ \| '_ ` _ \| |
+| (_| | (_) | | | | | | |
+ \__, |\___/|_| |_| |_|_|
+  __/ |                  
+ |___/                   
+
+  Type `gomi --help' to start
+
         EOS
     end
 end
